f00a58d7d18117410ae84337c71e9aef5512ab4e
N000188155 <lincaijie0102@fpg.com.tw>
2023-03-02 17:17:41 +0800

    bootstrap套用+註冊、登入 0302

---
+++ admin0222/Controllers/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using admin0222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace admin0222.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml", "_LayoutMember");
+        }
+
+    }
+}
